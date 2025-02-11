fb5d2d6c0236829a7ead000ac13b36c710e2cdb6
RaelDev <157400417+Raelfrances@users.noreply.github.com>
2025-02-11 23:04:51 +0000

    New commit

--- ContaTerminal.java
+++ ContaTerminal.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;  
+
 public class ContaTerminal{
-    Static 
+  public static void main(String[] args){
+    Scannner sc = new Scanner(System.in);
+
+    System.out.println("Informe o número da conta:  ");
+    int numeroConta = nextInt();
+
+    System.out.println("Informe o número da Agência:  ");
+    String numeroAgencia = next();
+
+    System.out.println("Informe o nome do cliente:  ");
+    String nomeCliente = nextLine();
+
+    System.out.println("Informe o saldo inicial:  ");
+    double saldoInicial = nextDouble();
+
+    String mensagem = " Olá" + nomeCliente + ",obrigado criar uma conta em nosso banco, " + "Sua Agência é: " + numeroAgencia + ",conta: " + numeroConta +" e seu saldo inicial é de: " + saldoInicial + ", e já está disponivel para saque."; 
+
+    System.out.println(mensagem);
+
+    scanner.close(); 
+        
+    }
 }
